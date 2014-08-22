f84ed292b0028aeeeb7ceaebca6e146a5cf68d77
Ashish Chandra <mail.ashishchandra@gmail.com>
2014-08-22 14:57:03 +0530

    Created a structure

---
+++ openstack_vm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Welcome Ashish "
+echo "Gearing up to create a customised environment for you."
+sleep(1)
+echo "Checking for internet connectivity"
+#Check for internet connectivity by pinging to Google.
+#         ping -c 4 www.google.com
+#If it works go ahead, else show a message that your network is not configured
+#
+echo "Updating packages"
+sudo apt-get update
+
+# Check for if "vim" and "git" is installed, if not then install them
+
+sudo apt-get install vim
+sudo apt-get install git
+
+# Configure vim
+echo "Configuring VIM"
+
+echo "Installing ipython"
+#Install ipython
+
+echo "Install and configure zsh"
+#Install zsh and configure it.
+
+
