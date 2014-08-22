cba96104a4a4ce847fe7ba57635f5b37a94b3c6c
Ashish Chandra <mail.ashishchandra@gmail.com>
2014-08-22 15:08:55 +0530

    Created a structure

--- openstack_vm.sh
+++ openstack_vm.sh
@@ -2,12 +2,18 @@
 
 echo "Welcome Ashish "
 echo "Gearing up to create a customised environment for you."
-sleep(1)
+sleep 1
 echo "Checking for internet connectivity"
 #Check for internet connectivity by pinging to Google.
 #         ping -c 4 www.google.com
 #If it works go ahead, else show a message that your network is not configured
 #
+ping -c 4 www.google.com
+if [ "$?" != 0 ]; then
+  echo "No internet connection! Please check your network settings"
+  exit 0 
+  fi
+
 echo "Updating packages"
 sudo apt-get update
 
